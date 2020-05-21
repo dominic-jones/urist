76b70f48fa9b45c2f0394982e145ba5ab3958da5
Dominic Jones <dojones@atlassian.com>
2020-05-21 16:27:26 +0900

    Actually logging the access log now

--- urist-access-logger/src/main/kotlin/org/dv/urist/AccessLogger.kt
+++ urist-access-logger/src/main/kotlin/org/dv/urist/AccessLogger.kt
@@ -1,10 +1,12 @@
 package org.dv.urist
 
+import org.slf4j.LoggerFactory
 import javax.servlet.http.HttpServletResponse
 
 class AccessLogger(
         private val uristApplicationProperties: UristApplicationProperties
 ) {
+    private val log = LoggerFactory.getLogger(AccessLogger::class.java)
 
     fun before() {
         UristSlf4j.withService(uristApplicationProperties.service)
@@ -12,6 +14,8 @@ class AccessLogger(
 
     fun after(response: HttpServletResponse) {
         UristSlf4j.withStatus(response.status)
+
+        log.info(uristApplicationProperties.accessLogMessage)
     }
 }
 
--- urist-access-logger/src/main/kotlin/org/dv/urist/UristApplicationProperties.kt
+++ urist-access-logger/src/main/kotlin/org/dv/urist/UristApplicationProperties.kt
@@ -3,8 +3,11 @@ package org.dv.urist
 import org.springframework.boot.context.properties.ConfigurationProperties
 import javax.validation.constraints.NotEmpty
 
+const val DEFAULT_ACCESS_LOG_MESSAGE = "ACCESS_LOG"
+
 @ConfigurationProperties("urist")
 data class UristApplicationProperties(
         @NotEmpty
-        val service: String
+        val service: String,
+        val accessLogMessage: String = DEFAULT_ACCESS_LOG_MESSAGE
 )
