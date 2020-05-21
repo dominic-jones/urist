8d17dd58f2ea2677509f16d00bde908f6142e9d4
Dominic Jones <dojones@atlassian.com>
2020-05-21 16:07:49 +0900

    Adding mandatory validation to ServiceId

--- urist-access-logger/src/main/kotlin/org/dv/urist/UristApplicationProperties.kt
+++ urist-access-logger/src/main/kotlin/org/dv/urist/UristApplicationProperties.kt
@@ -1,8 +1,10 @@
 package org.dv.urist
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import javax.validation.constraints.NotEmpty
 
 @ConfigurationProperties("urist")
 data class UristApplicationProperties(
+        @NotEmpty
         val service: String
 )
